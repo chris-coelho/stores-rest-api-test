b750787e0727d51017c7dc9550a348fcaf0eb4b7
Cristovao Coelho <chriscoelho@MacBook-Air-de-Cristovao.local>
2018-01-13 23:51:51 -0200

    Updated tests files.

--- tests/unit/models/item_test.py
+++ tests/unit/models/item_test.py
@@ -1,19 +1,21 @@
-from unittest import TestCase
-
 from models.item import ItemModel
+from tests.base_test import BaseTest
 
 
-class ItemTest(TestCase):
+class ItemTest(BaseTest):
     def test_create_item(self):
-        item = ItemModel('test', 19.99)
+        item = ItemModel('test', 19.99, 1)
 
         self.assertEqual(item.name, 'test',
                          "The name of the item after creation does not equal the constructor argument.")
         self.assertEqual(item.price, 19.99,
                          "The price of the item after creation does not equal the constructor argument.")
+        self.assertEqual(item.store_id, 1,
+                         "The store_id of the item after creation does not equal the constructor argument.")
+        self.assertIsNone(item.store, "The item's store was not None even though the store was not created.")
 
     def test_item_json(self):
-        item = ItemModel('test', 19.99)
+        item = ItemModel('test', 19.99, 1)
         expected = {
             'name': 'test',
             'price': 19.99
--- tests/unit/models/test_user.py
+++
@@ -1,3 +0,0 @@
-from models.user import UserModel
-from tests.unit.unit_base_test import UnitBaseTest
-
